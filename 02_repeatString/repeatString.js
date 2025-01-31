951aa4e6ef39c0a9bbe327cdaa5eb061447707bd
Iain Leslie <4786415+IDL2013@users.noreply.github.com>
2025-01-31 13:05:08 +0000

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string,count) {
+    if(!string){string=""};
+    count = parseInt(count);
+    if(count>0){let fullString = string;
+    for (i=1;i<count;i++) {
+    fullString += string};
+    return fullString;
+}
+else if (count === 0) {return ""}
+else {return "ERROR"};
 };
 
 // Do not edit below this line
