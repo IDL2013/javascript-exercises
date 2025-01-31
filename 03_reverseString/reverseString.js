189d008e456ac0da5ccdf64661ecda3804d465ae
Iain Leslie <4786415+IDL2013@users.noreply.github.com>
2025-01-31 17:55:24 +0000

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    stringArray = Array.from(string);
+    let revString = [];
+    for(i=0; i<string.length; i++){
+        revString += stringArray.pop()
+    }
+    if(string.length<1){return ''};
+    return revString;
 };
 
 // Do not edit below this line
