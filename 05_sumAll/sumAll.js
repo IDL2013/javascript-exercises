383e594e8f32f45b8b819d219315ef75020f7287
Iain Leslie <4786415+IDL2013@users.noreply.github.com>
2025-02-03 20:39:48 +0000

    ATTEMPT first stages of problem

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
+const sumAll = function(x,y) { 
+    min = Math.min(x,y);
+    max = Math.max(x,y);
+    let val = 0;
+    for(i=min;i<=max;i++){
+        val += i
+    }
+    return val;}
 
-};
 
 // Do not edit below this line
 module.exports = sumAll;
