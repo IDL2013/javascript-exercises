9994beccc4ddd2d5d05e7a6a8225f12abbc50c84
Iain Leslie <4786415+IDL2013@users.noreply.github.com>
2025-02-03 17:04:32 +0000

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-};
-
+const removeFromArray = function(array,...entryToRemove) {
+    const filtered =  array.filter(array => !entryToRemove.includes(array));
+    return filtered;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
