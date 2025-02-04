164b0e41ae6f7030dc63d7902796b42fb7f19414
Iain Leslie <4786415+IDL2013@users.noreply.github.com>
2025-02-04 15:19:00 +0000

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let century = Number.isInteger(year / 100);
+    let fourHundred = Number.isInteger(year / 400);
 
+    
+    if(year % 4==0 && century==false ){
+        return true;
+    } else if (year % 4==0 && fourHundred==true){
+        return true;
+    }
+    else {return false;}
 };
 
 // Do not edit below this line
