d02333da3768e4a497c1ea6e03fda780924b1166
Iain Leslie <4786415+IDL2013@users.noreply.github.com>
2025-02-04 13:36:15 +0000

    Complete Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,21 @@
 const sumAll = function(x,y) { 
-    min = Math.min(x,y);
-    max = Math.max(x,y);
-    let val = 0;
-    for(i=min;i<=max;i++){
-        val += i
+    const sumXY = function(x,y) {min = Math.min(x,y);
+        max = Math.max(x,y);
+        let val = 0;
+        for(i=min;i<=max;i++){
+            val += i
+        }
+        return val;}
+    
+    if(Math.sign(x)==1 && Math.sign(y)==1){
+        if(Number.isInteger(x) && Number.isInteger(y)){
+        return sumXY(x,y)}
+        else {return "ERROR"};
     }
-    return val;}
+    else {return "ERROR"};
+    
+    }
+    
 
 
 // Do not edit below this line
