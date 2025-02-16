129acc9e87e1a090fb4c6a86f0754ddd5f2716d6
Iain Leslie <4786415+IDL2013@users.noreply.github.com>
2025-02-16 10:26:32 +0000

    Complete Ex 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    let regEx = /[^A-Za-z0-9_]/g;
+    let arrForward = input.toLowerCase().replace(regEx,'');
+    let arrReverse = arrForward.split('').reverse().join('');
 
-};
+    if(arrForward === arrReverse){
+        return true
+    } else if (arrForward != arrReverse){return false};
+    }
+;
 
 // Do not edit below this line
 module.exports = palindromes;
