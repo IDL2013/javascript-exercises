1a2059b0f0d154edc234abd5bfb99a62a0b5aed9
Iain Leslie <4786415+IDL2013@users.noreply.github.com>
2025-02-16 10:45:48 +0000

    Complete ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
 
+    input = parseInt(input);
+    if(input<0){return "OOPS"}
+    else if (input>0){
+        let goldenRule= ((1.618034**input) - ((1-1.618034)**input))/Math.sqrt(5)
+        return Math.round(goldenRule);
+    }
+    else if (input === 0){return 0;}
+    else {return "OOPS"};
 };
 
 // Do not edit below this line
