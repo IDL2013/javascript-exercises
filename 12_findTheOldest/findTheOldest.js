993289de702fe027a92e66bf90d1b10f67d31e57
Iain Leslie <4786415+IDL2013@users.noreply.github.com>
2025-02-20 21:12:25 +0000

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const getAge = function(death,birth){
+    if(!death){
+        const todayDate = new Date();
+        death = todayDate.getFullYear();
+    }
+    death = parseInt(death);
+    birth = parseInt(birth);
+    return death - birth;
+}
 
-};
+const findTheOldest = function(people) {
+
+    // REDUCE array to the highest age
+    return people.reduce((previous, current) => { 
+        const previousAge = getAge(previous.yearOfDeath,previous.yearOfBirth);
+        const currentAge = getAge(current.yearOfDeath,current.yearOfBirth);
+    return previousAge < currentAge ? current : previous;
+    })
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
